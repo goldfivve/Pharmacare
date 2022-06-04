843dd9ed6d68f84543074642bbee30ad599175b0
Dominika <57232831+goldfivve@users.noreply.github.com>
2022-06-04 18:11:13 +0200

    add DrugRepository

---
+++ Pharmacare.Api/Repositories/Contracts/IDrugRepository.cs
@@ -0,0 +1,12 @@
+﻿using Pharmacare.Entities;
+
+namespace Pharmacare.Api.Repositories.Contracts
+{
+    public interface IDrugRepository
+    {
+        Task<IEnumerable<Drug>> GetItems();
+        Task<IEnumerable<Category>> GetCategories();
+        Task<Drug> GetDrugById(Guid id);
+        Task<Drug> GetCategoryById(Guid id);
+    }
+}
---
+++ Pharmacare.Api/Repositories/DrugRepository.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pharmacare.Api.Repositories.Contracts;
+using Pharmacare.Data;
+using Pharmacare.Entities;
+
+namespace Pharmacare.Api.Repositories
+{
+    public class DrugRepository : IDrugRepository
+    {
+        private readonly PharmacareDbContext _pharmacareDbContext;
+
+        public DrugRepository(PharmacareDbContext pharmacareDbContext)
+        {
+            this._pharmacareDbContext = pharmacareDbContext;
+        }
+        public async Task<IEnumerable<Drug>> GetItems()
+        {
+            var drugs = await this._pharmacareDbContext.Drugs.ToListAsync();
+
+            return drugs;
+        }
+
+        public async Task<IEnumerable<Category>> GetCategories()
+        {
+            var categories = await this._pharmacareDbContext.Categories.ToListAsync();
+
+            return categories;
+        }
+
+        public Task<Drug> GetDrugById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Drug> GetCategoryById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
